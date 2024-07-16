18183eea252117432614e6d0dba61782e8e96ec9
Nathanael Lima <nathanaellimacpc@gmail.com>
2024-07-16 18:58:51 -0300

    Initial commit

---
+++ sum.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(soma(15, 15))
+}
+
+func soma(a int, b int) int {
+	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func times(a int, b int) int {
+	return a * b
+}
+
+func divided(a int, b int) int {
+	return a / b
+}
